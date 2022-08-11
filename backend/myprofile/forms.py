7f0851e3f2f215f378557c27da7ef3501bcd6c03
flood1991 <drink2getdrunk91@gmail.com>
2022-08-11 16:09:38 +0300

    add_edit_profile

---
+++ backend/myprofile/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.forms import ModelForm
+from db.models import User
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name',
+                  'last_name', 'phone', 'email',)
+
+        labels = {'username': '',
+                  'first_name': '',
+                  'last_name': '',
+                  'phone': '',
+                  'email': '',
+                  }
+
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control-3 ', 'placeholder': 'Username'}),
+                   'first_name': forms.TextInput(attrs={'class': 'form-control-3', 'placeholder': 'First_name'}),
+                    'last_name': forms.TextInput(attrs={'class': 'form-control-3', 'placeholder': 'Last_name'}),
+                    'phone': forms.TextInput(attrs={'class': 'form-control-3', 'placeholder': 'Phone'}),
+                    'email': forms.TextInput(attrs={'class': 'form-control-3', 'placeholder': 'Email'}),
+                 }
+
+
+class UserAvatar(ModelForm):
+    class Meta:
+        model = User
+        fields = ('avatar',)
