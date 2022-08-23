bcaabe20c39ad39a4b42811158a3910caf38ef05
yarkoniukvoh <yar.koniukhov@gmail.com>
2022-08-23 16:49:14 +0300

    delete and edit workouts

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, redirect
 from db.models import Workouts
 from .forms import WorkoutsForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def my_workout(request):
     workout = Workouts.objects.filter(user=request.user).order_by('-date_create')
     return render(request, 'workouts/workouts.html', {'workout': workout})
+
+
+def detail_workout(request, pk):
+    workout = Workouts.objects.get(pk=pk)
+    return render(request, 'workouts/detail_workout.html', {'workout': workout})
+
+
+class WorkoutUpdatelView(UpdateView):
+    model = Workouts
+    template_name = 'workouts/create_workout.html'
+    form_class = WorkoutsForm
+
+
+class WorkoutDeletelView(DeleteView):
+    model = Workouts
+    success_url = '/workout/'
+    template_name = 'workouts/delete_workout.html'
 
 
 def create_workout(request):
