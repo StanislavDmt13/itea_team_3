33f8f27de3d2dcce021652174dc3767d67aa4919
nastiakh1301 <nastia.1092.ch@gmail.com>
2022-08-29 23:17:30 +0300

    added redirect for homepage

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView, UpdateView, FormView, ListView
-
-# Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World!</h1>")
-
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class HomepageView(TemplateView):
     template_name = 'home.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            self.template_name = 'profile.html'
         return super().get_context_data(**kwargs)
 
