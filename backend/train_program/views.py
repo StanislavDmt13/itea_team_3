1b1c5c12ce681dff974463c4051f433ef21c7d2d
flood1991 <drink2getdrunk91@gmail.com>
2022-08-30 18:09:42 +0300

    add api train program

--- backend/train_program/views.py
+++ backend/train_program/views.py
@@ -1,25 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_list_or_404
-from django.views.generic import ListView, DetailView
-from db.models import Task, Category
+from django.views.generic import ListView, DetailView, CreateView
+from db import models
+from . import forms
 
 
 class TaskDetailView(DetailView):
-    model = Task
+    model = models.Task
     template_name = 'programs/task.html'
     slug_url_kwarg = 'task_slug'
     context_object_name = 'task'
 
 
 class TaskByCategoryView(ListView):
-    model = Task
+    model = models.Task
     context_object_name = 'task_list'
     template_name = 'programs/task_list.html'
 
     def get_queryset(self):
-        return Task.objects.filter(category__slug=self.kwargs['cat_slug'])
+        return models.Task.objects.filter(category__slug=self.kwargs['cat_slug'])
 
 
 class CategoriesView(ListView):
-    model = Category
+    model = models.Category
     context_object_name = 'categories'
+
+
+class CreateTrain(CreateView):
+    model = models.TrainProgram
+    form_class = forms.TrainForm
+    template_name = 'programs/train_program.html'
